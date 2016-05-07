53223da77138e22a43dc3d7027a94b083c0daa38
Marcus <marcus.pettersen@gmail.com>
2016-05-07 08:47:21 +0200

    Some restructuring.

--- Monolith/Signals/ISignal.cs
+++ Monolith/Signals/ISignal.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Monolith
+namespace Monolith.Signals
 {
     public delegate void SignalEventHandler(ISignal signal);
 
