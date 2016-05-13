9bde232d709d936e62dcc45155c284b61ddbc6dd
Marcus Pettersen <mcp@appeartv.com>
2016-05-13 12:43:17 +0200

    REST api now supplies all devices.

---
+++ Monolith/Devices/DeviceState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolith.Plugins
+{
+    public class DeviceState : Framework.ObjectBase
+    {
+        public enum States : int
+        {
+            Uninitialized = 0,
+            Initialized,
+            Offline,
+            Online
+        }
+
+        private Framework.AttributeBase<States> state;
+
+        public States State
+        {
+            get { return this.state.Value; }
+            set { this.state.Value = value; }
+        }
+
+		public DeviceState(string identifier)
+            : base(identifier)
+        {
+            this.state = new Framework.AttributeBase<States>(this);
+        }
+    }
+}
