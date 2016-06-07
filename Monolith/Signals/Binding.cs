cbaa8c5b770b3ef305ed17ae06c455a5cb816863
Marcus <marcus.pettersen@gmail.com>
2016-06-07 21:08:16 +0200

    Started adding basic signal binging.

---
+++ Monolith/Signals/Binding.cs
@@ -0,0 +1,41 @@
+﻿using Monolith.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolith.Signals
+{
+    class Binding<U, V>
+    {
+        public enum BindingMode
+        {
+            OneWay,
+            TwoWay
+        }
+
+        public Signal<U> First { get; private set; }
+        public Signal<V> Second { get; private set; }
+        public BindingMode Mode { get; private set; }
+
+        public Binding(Signal<U> u, Signal<V> v, BindingMode mode)
+        {
+            this.First = u;
+            this.Second = v;
+            this.Mode = mode;
+
+            this.First.InnerState.AttributeChanged += (IAttribute a) => { sync(this.First, this.Second); };
+
+            if(this.Mode == BindingMode.TwoWay)
+            {
+                this.Second.InnerState.AttributeChanged += (IAttribute a) => { sync(this.Second, this.First); };
+            }
+        }
+
+        private void sync<A, B>(Signal<A> from, Signal<B> to)
+        {
+            to.State = from.State;
+        }
+    }
+}
