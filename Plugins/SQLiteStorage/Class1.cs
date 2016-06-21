c6a7daf95ab280c5d843e792743a2c80e5168ec2
Marcus <marcus.pettersen@gmail.com>
2016-06-21 21:32:46 +0200

    Started basic Signal storage.

--- Plugins/SQLiteStorage/Class1.cs
+++ Plugins/SQLiteStorage/Class1.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using Monolith.Framework;
+using Monolith.Signals;
+using System.Reflection;
 
 namespace Monolith.Plugins.SQLiteStorage
 {
-    public class Class1
+    public class SignalsStorage
     {
+        private Channel channel;
+
+        public SignalsStorage()
+        {
+            this.channel = Manager.Instance.create("Signals");
+            this.channel.subscribe(typeof(IObject), this.newObject);
+        }
+
+        private void newObject(Channel c, IObject obj)
+        {
+            foreach(PropertyInfo p in obj.GetType().GetProperties())
+            {
+                if(p.CanRead && p.CanWrite)
+            }
+        }
     }
 }
