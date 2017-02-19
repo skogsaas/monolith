34bbac33c92da39dc00c78be0c940a8f1ab8863e
Marcus <marcus.pettersen@gmail.com>
2017-02-19 11:41:56 +0100

    --amend

---
+++ Monolith/Configuration/IIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using Skogsaas.Legion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skogsaas.Monolith.Configuration
+{
+    public interface IIdentifier : IObject
+    {
+        string Typename { get; set; }
+        string Plugin { get; set; }
+    }
+}
---
+++ Monolith/Configuration/Utilitites.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skogsaas.Monolith.Configuration
+{
+    public class Utilities
+    {
+        public static void PrepareConfiguration(IIdentifier identifier)
+        {
+            identifier.Typename = identifier.GetInterface().FullName;
+            identifier.Plugin = identifier.GetInterface().Namespace;
+        }
+    }
+}
